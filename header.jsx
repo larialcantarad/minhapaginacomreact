e46cc0591f5fad337d263f01be79efa49cd403dd
larialcantarad <151067062+larialcantarad@users.noreply.github.com>
2023-12-10 15:58:48 -0300

    Update header.jsx

--- header.jsx
+++ header.jsx
@@ -13,35 +13,15 @@ function header(){
     return (
         <>
         
-        {/* <h2 className="ofertas">Ofertas em dezembro</h2> */}
+       
        <nav className = "meuNavbar">
         <h2 className="nomedosite">Larissa.dev</h2>
-        {/* esse link que esta ferrando, faz ficar em tela branca */}
-        {/* <Link to ="/">home</Link> */}
-        {/* mas a tag a também não esta funcionando */}
-        {/* <BrowserRouter>
-          <Routes>
-               <Route path = "/" element = {<a href="../Pages/Sobre" target = "_blank">Sobre</a>}></Route>
-          </Routes>
-        
-        </BrowserRouter> */}
+       
         <a href='../Pages/Home' target = "_blank">Home</a>
         <a href="../Pages/Sobre" target = "_blank">Sobre</a>
         <a href="../Pages/Projetos" target = "_blank">Projetos</a>
         <a href='../Pages/Contato' target = "_blank">Contato</a>
        </nav> 
-        {/* <nav>
-            <Link to="/feminino">Feminino</Link>
-            <Link to="/masculino">Masculino</Link>
-            <Link to="/infantil">Infantil</Link>
-            <Link to="/sale">Sale</Link>
-        </nav>
-         */}
-        {/* <div>
-            <Carrossel></Carrossel>
-        </div>
-        */}
-        
         
         </>
     )
